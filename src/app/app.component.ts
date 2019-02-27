14817983367788b5807042b29e3ff9de380aea1e
Sebastian Rotar <rotar.seby1@gmail.com>
2019-02-27 10:47:25 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-egn';
+  emailIsValid: boolean = false;
+  passwordIsValid: boolean = false;
+
+  instantValidationEmail(event: any) {
+    console.log(event);
+    if (event.srcElement.classList.contains("ng-valid")) {
+      this.emailIsValid = true;
+      event.srcElement.classList.remove("invalid-input");
+    } else {
+      this.emailIsValid = false;
+      event.srcElement.classList.add("invalid-input");
+    }
+  }
+
+  instantValidationPassword(event: any) {
+    console.log(event);
+    if (event.srcElement.classList.contains("ng-valid")) {
+      this.passwordIsValid = true;
+      event.srcElement.classList.remove("invalid-input");
+    } else {
+      this.passwordIsValid = false;
+      event.srcElement.classList.add("invalid-input");
+    }
+  }
+
+  onLogin(loginForm: NgForm) {
+    console.log(loginForm);
+    loginForm.reset();
+  }
 }
