36540bc65588fe2c6dda02e160f7f609410b759b
Sebastian Rotar <rotar.seby1@gmail.com>
2019-03-04 16:49:16 +0100

    events sorted done

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -10,9 +10,9 @@ export class ArraySortPipe implements PipeTransform {
     }
 
     array.sort((a: any, b: any) => {
-      if (a[field] > b[field]) {
+      if (a[field] < b[field]) {
         return -1;
-      } else if (a[field] < b[field]) {
+      } else if (a[field] > b[field]) {
         return 1;
       } else {
         return 0;
