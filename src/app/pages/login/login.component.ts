cbbb0b11d92e0a6555c2f41d28d47121ad29ad75
Sebastian Rotar <rotar.seby1@gmail.com>
2019-06-06 02:00:07 +0200

    last commit done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -57,7 +57,7 @@ export class LoginComponent implements OnInit {
         var actionCodeSettings = {
           // URL you want to redirect back to. The domain (www.example.com) for this
           // URL must be whitelisted in the Firebase Console.
-          url: "http://quickcocktails.dk/kea/final-project-egn/",
+          url: "http://quickcocktails.dk/kea/egn-project-kea/",
           handleCodeInApp: true
         };
         this.authService.activateAccount(
